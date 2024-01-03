bace7148a7754248dbe055f029555f9fd281967c
Claudio Rivera Bravo <riverabravocl@outlook.com>
2024-01-03 16:43:12 -0300

    Listado de notas: configuración inicial

---
+++ resources/js/Pages/Notes/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Modulo de Notas
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1">
+                        <div class="p3-4 sm:px-0">
+                            <h3 class="text-lg text-gray-900">Listado de Notas</h3>
+                            <p class="text-sm text-gray-600">Toma el registro correcto y ejecuta cualquier función (ver, editar o eliminar)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
